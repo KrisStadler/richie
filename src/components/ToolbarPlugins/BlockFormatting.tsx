d888420bc83d5915c82bc167c5c413363e936644
Kris Stadler <21295730+KrisStadler@users.noreply.github.com>
2023-11-01 08:46:28 +0200

    Toolbar WIP'ing

--- src/components/ToolbarPlugins/BlockFormatting.tsx
+++ src/components/ToolbarPlugins/BlockFormatting.tsx
@@ -1,87 +1,85 @@
-import {useLexicalComposerContext} from "@lexical/react/LexicalComposerContext";
-import React, {useEffect, useState} from "react";
-import {TextFormatType} from "lexical/nodes/LexicalTextNode";
-import {$getSelection, $isRangeSelection} from "lexical";
-import {ToggleButton, ToggleButtonGroup} from "@mui/material";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import React, { useEffect, useState } from "react";
+import { TextFormatType } from "lexical/nodes/LexicalTextNode";
+import { $getSelection, $isRangeSelection } from "lexical";
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
 import FormatBoldIcon from "@mui/icons-material/FormatBold";
 import FormatItalicIcon from "@mui/icons-material/FormatItalic";
 import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
-import FormatColorFillIcon from "@mui/icons-material/FormatColorFill";
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 interface Props {
-    isBold: boolean;
-    isItalic: boolean;
-    isUnderline: boolean;
+  isBold: boolean;
+  isItalic: boolean;
+  isUnderline: boolean;
 }
 
-const BlockFormatting = ({isBold, isItalic, isUnderline}: Props) => {
-    const [editor] = useLexicalComposerContext();
-    const [formats, setFormats] = useState<Array<TextFormatType>>([]);
+const BlockFormatting = ({ isBold, isItalic, isUnderline }: Props) => {
+  const [editor] = useLexicalComposerContext();
+  const [formats, setFormats] = useState<Array<TextFormatType>>([]);
 
-    useEffect(() => {
-        if (isBold && !formats.includes('bold')) {
-            setFormats([...formats,'bold']);
-        } else if (!isBold && formats.includes('bold')) {
-            setFormats(formats.filter((value) => value !== 'bold'));
-        }
-    }, [isBold, formats])
+  useEffect(() => {
+    if (isBold && !formats.includes("bold")) {
+      setFormats([...formats, "bold"]);
+    } else if (!isBold && formats.includes("bold")) {
+      setFormats(formats.filter((value) => value !== "bold"));
+    }
+  }, [isBold, formats]);
 
-    useEffect(() => {
-        if (isItalic && !formats.includes('italic')) {
-            setFormats([...formats,'italic']);
-        } else if (!isItalic && formats.includes('italic')) {
-            setFormats(formats.filter((value) => value !== 'italic'));
-        }
-    }, [isItalic, formats])
+  useEffect(() => {
+    if (isItalic && !formats.includes("italic")) {
+      setFormats([...formats, "italic"]);
+    } else if (!isItalic && formats.includes("italic")) {
+      setFormats(formats.filter((value) => value !== "italic"));
+    }
+  }, [isItalic, formats]);
 
-    useEffect(() => {
-        if (isUnderline && !formats.includes('underline')) {
-            setFormats([...formats,'underline']);
-        } else if (!isUnderline && formats.includes('underline')) {
-            setFormats(formats.filter((value) => value !== 'underline'));
-        }
-    }, [isUnderline, formats])
+  useEffect(() => {
+    if (isUnderline && !formats.includes("underline")) {
+      setFormats([...formats, "underline"]);
+    } else if (!isUnderline && formats.includes("underline")) {
+      setFormats(formats.filter((value) => value !== "underline"));
+    }
+  }, [isUnderline, formats]);
 
-    const updateEditorTextFormat = (format: TextFormatType) => {
-        editor.update(() => {
-            const selection = $getSelection();
-            if ($isRangeSelection(selection)) {
-                selection.formatText(format);
-            }
-        });
-    };
-    const handleFormat = (_: React.MouseEvent, newFormats: Array<TextFormatType>) => {
-        if (newFormats.length < formats.length) {
-            const format = formats.filter((value) => !newFormats.includes(value))[0];
-            updateEditorTextFormat(format as TextFormatType);
-        } else {
-            const format = newFormats.filter((value) => !formats.includes(value))[0];
-            updateEditorTextFormat(format as TextFormatType);
-        }
-    };
+  const updateEditorTextFormat = (format: TextFormatType) => {
+    editor.update(() => {
+      const selection = $getSelection();
+      if ($isRangeSelection(selection)) {
+        selection.formatText(format);
+      }
+    });
+  };
+  const handleFormat = (
+    _: React.MouseEvent,
+    newFormats: Array<TextFormatType>,
+  ) => {
+    if (newFormats.length < formats.length) {
+      const format = formats.filter((value) => !newFormats.includes(value))[0];
+      updateEditorTextFormat(format as TextFormatType);
+    } else {
+      const format = newFormats.filter((value) => !formats.includes(value))[0];
+      updateEditorTextFormat(format as TextFormatType);
+    }
+  };
 
-    return (
-        <ToggleButtonGroup
-            value={formats}
-            onChange={handleFormat}
-            aria-label="text formatting"
-        >
-            <ToggleButton value="bold" aria-label="bold">
-                <FormatBoldIcon />
-            </ToggleButton>
-            <ToggleButton value="italic" aria-label="italic">
-                <FormatItalicIcon />
-            </ToggleButton>
-            <ToggleButton value="underline" aria-label="underline">
-                <FormatUnderlinedIcon />
-            </ToggleButton>
-            <ToggleButton value="color" aria-label="color">
-                <FormatColorFillIcon />
-                <ArrowDropDownIcon />
-            </ToggleButton>
-        </ToggleButtonGroup>
-    );
-}
+  return (
+    <ToggleButtonGroup
+      value={formats}
+      onChange={handleFormat}
+      aria-label="text formatting"
+      size="small"
+    >
+      <ToggleButton value="bold" aria-label="bold">
+        <FormatBoldIcon />
+      </ToggleButton>
+      <ToggleButton value="italic" aria-label="italic">
+        <FormatItalicIcon />
+      </ToggleButton>
+      <ToggleButton value="underline" aria-label="underline">
+        <FormatUnderlinedIcon />
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
+};
 
-export default BlockFormatting
+export default BlockFormatting;
--- src/components/ToolbarPlugins/TextAlignment.tsx
+++ src/components/ToolbarPlugins/TextAlignment.tsx
@@ -1,45 +1,72 @@
-import {useLexicalComposerContext} from "@lexical/react/LexicalComposerContext";
-import React, {useState} from "react";
-import {ElementFormatType} from "lexical/nodes/LexicalElementNode";
-import {FORMAT_ELEMENT_COMMAND} from "lexical";
-import {ToggleButton, ToggleButtonGroup} from "@mui/material";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import React, { useState } from "react";
+import { ElementFormatType } from "lexical/nodes/LexicalElementNode";
+import { FORMAT_ELEMENT_COMMAND } from "lexical";
+import { IconButton, ListItemIcon, Menu, MenuItem } from "@mui/material";
 import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
 import FormatAlignCenterIcon from "@mui/icons-material/FormatAlignCenter";
 import FormatAlignRightIcon from "@mui/icons-material/FormatAlignRight";
 import FormatAlignJustifyIcon from "@mui/icons-material/FormatAlignJustify";
 
 const TextAlignment = () => {
-    const [editor] = useLexicalComposerContext();
-    const [alignment, setAlignment] = useState<ElementFormatType>('');
-    const updateEditorTextFormat = (format: ElementFormatType) => {
-        editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, format);
-    };
-    const handleAlignment = (_: React.MouseEvent, newFormat: ElementFormatType) => {
-        setAlignment(newFormat)
-        updateEditorTextFormat(newFormat);
-    };
+  const [editor] = useLexicalComposerContext();
 
-    return (
-        <ToggleButtonGroup
-            value={alignment}
-            exclusive
-            onChange={handleAlignment}
-            aria-label="text alignment"
-        >
-            <ToggleButton value="left" aria-label="left aligned">
-                <FormatAlignLeftIcon />
-            </ToggleButton>
-            <ToggleButton value="center" aria-label="centered">
-                <FormatAlignCenterIcon />
-            </ToggleButton>
-            <ToggleButton value="right" aria-label="right aligned">
-                <FormatAlignRightIcon />
-            </ToggleButton>
-            <ToggleButton value="justify" aria-label="justified">
-                <FormatAlignJustifyIcon />
-            </ToggleButton>
-        </ToggleButtonGroup>
-    );
-}
+  const [anchorEl, setAnchorEl] = useState<null | HTMLButtonElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const updateEditorTextFormat = (format: ElementFormatType) => {
+    editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, format);
+  };
+
+  const handleAlignment = (newFormat: ElementFormatType) => {
+    updateEditorTextFormat(newFormat);
+    handleClose();
+  };
+
+  return (
+    <div>
+      <IconButton
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        <FormatAlignJustifyIcon />
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={() => handleAlignment("left")}>
+          <ListItemIcon>
+            <FormatAlignLeftIcon />
+          </ListItemIcon>
+        </MenuItem>
+        <MenuItem onClick={() => handleAlignment("center")}>
+          <ListItemIcon>
+            <FormatAlignCenterIcon />
+          </ListItemIcon>
+        </MenuItem>
+        <MenuItem onClick={() => handleAlignment("right")}>
+          <ListItemIcon>
+            <FormatAlignRightIcon />
+          </ListItemIcon>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+};
 
 export default TextAlignment;
